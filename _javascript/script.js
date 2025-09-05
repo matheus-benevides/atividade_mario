51a406ac9660255cd7ebe7a0a437f26f6b840013
Matheus G Benevides <matheus.g.benevides@aluno.senai.br>
2025-09-05 15:12:48 -0300

    Adicoes

--- _javascript/script.js
+++ _javascript/script.js
@@ -1,38 +1,55 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
 
-const jump = ()=>{
+const heart1 = document.querySelector(".heart");
+const heart2 = document.querySelector(".heart1");
+const heart3 = document.querySelector(".heart2");
+
+let lifes = 3;
+
+const jump = () => {
     mario.classList.add("jump");
-    setTimeout(()=>{
-        mario.classList.remove();
-    },500);
+    setTimeout(() => {
+        mario.classList.remove("jump");
+    }, 500);
 }
 
-//Explicação
-//function jump(){
-//   mario.classList.add('jump');
-//   setTimeout(function(){
-//    mario.classList.remove('jump');
-//   },500);
-// }
+let pontos = 0;
 
-const loop = setInterval(()=>{
+const loop = setInterval(() => {
     console.log("loop");
-});
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
 
-const pipePosition = pipe.offsetLeft;
-const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
+    pontos++;
 
-if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
-    pipe.style.animation = "none";
-    pipe.style.left = `${pipePosition}px`;
-    mario.style.animation = "none";
-    mario.style.bottom = `${marioPosition}px`;
-    
-    mario.src = "_imagens/game-over.png";
-    mario.style.width = "75px";
-    mario.style.marginLeft = "50px";
+    document.getElementById("pontos").innerHTML = pontos;
 
-    clearInterval(loop);
-}
-document.addEventListener("keydown",jump);
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80 && lifes == 0) {
+        pipe.style.animation = "none";
+        pipe.style.left = `${pipePosition}px`;
+        mario.style.animation = "none";
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = "_imagens/game-over.png";
+        mario.style.width = "75px";
+        mario.style.marginLeft = "50px";
+
+        clearInterval(loop);
+    }
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80 && lifes == 3){
+        lifes -= 1;
+        heart3.style.display = "none";
+    }
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80 && lifes == 2){
+        lifes -= 1;
+        heart2.style.display = "none";
+    }
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80 && lifes == 1){
+        lifes -= 1;
+        heart1.style.display = "none";
+    }
+
+}, 10);
+
+document.addEventListener("keydown", jump);
